86eb807d8c47b5962d3174ce337deb9278d69729
AshotManukyan <ashotm0571@gmail.com>
2025-02-21 15:23:12 +0100

    test: Add test for children inclusion in family quotient
    Add a test to verify the inclusion of children in the family quotient calculation.

---
+++ source/Tax.Simulator.Tests/CalculsImpots/ImpotsEnfantsACharge.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+
+namespace Tax.Simulator.Tests.CalculsImpots;
+
+public class ImpotsEnfantsACharge
+{
+    [Fact(DisplayName = "Calcul des impôts selon le nombre d'enfants à charge")]
+    public void CalculSelonNombreEnfants()
+    {
+        string situationFamiliale = "Marié/Pacsé";
+        int salaireMensuelPrincipal = 2000;
+        int salaireMensuelConjoint = 2500;
+        int nombreEnfants = 0;
+        decimal resultat = Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+        resultat.Should().Be(4043.90m);
+
+        nombreEnfants = 5;
+        resultat = Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+        resultat.Should().Be(878.62m);
+
+        nombreEnfants = -2;
+        Action action = () => Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelConjoint,
+            salaireMensuelPrincipal,
+            nombreEnfants
+        );
+
+        action.Should().Throw<ArgumentException>();
+    }
+}
