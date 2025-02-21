26802655e0f97700f4d04367ed8c75c6213b6be5
AshotManukyan <ashotm0571@gmail.com>
2025-02-21 15:23:45 +0100

    test: Add test for user input validation of the simulator
    Add a test to verify user input validation functionality.

---
+++ source/Tax.Simulator.Tests/Validation/SimulateurShould.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+
+namespace Tax.Simulator.Tests.Validation;
+
+public class SimulateurShould
+{
+    [Fact(DisplayName = "Situation familiale invalide")]
+    public void SituationFamilialeInvalide()
+    {
+        Action action = () => Simulateur.CalculerImpotsAnnuel(
+            "Divorcé",
+            3000,
+            0,
+            2
+        );
+
+        action.Should().Throw<ArgumentException>().WithMessage("Situation familiale invalide.");
+    }
+
+    [Fact(DisplayName = "Salaire mensuel négatif")]
+    public void SalaireMensuelNegatif()
+    {
+        Action action = () => Simulateur.CalculerImpotsAnnuel(
+            "Célibataire",
+            -3000,
+            0,
+            2
+        );
+
+        action.Should().Throw<ArgumentException>().WithMessage("Les salaires doivent être positifs.");
+    }
+
+    [Fact(DisplayName = "Nombre enfants négatif")]
+    public void NombreEnfantsNegatif()
+    {
+        Action action = () => Simulateur.CalculerImpotsAnnuel(
+            "Marié/Pacsé",
+            3000,
+            2000,
+            -2
+        );
+
+        action.Should().Throw<ArgumentException>().WithMessage("Le nombre d'enfants ne peut pas être négatif.");
+    }
+}
