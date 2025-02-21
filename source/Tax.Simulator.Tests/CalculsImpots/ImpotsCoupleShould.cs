ca44de4fc9ebfa38db4109a3ea3b138c4ebe614e
AshotManukyan <ashotm0571@gmail.com>
2025-02-21 15:22:44 +0100

    test: Add tax calculation test for married couple or civil union
    Add a test to verify the tax calculation functionality for a married or civil union couple depending on different parameters such as having kids or not..

---
+++ source/Tax.Simulator.Tests/CalculsImpots/ImpotsCoupleShould.cs
@@ -0,0 +1,105 @@
+﻿using FluentAssertions;
+
+namespace Tax.Simulator.Tests.CalculsImpots;
+
+public class ImpotsCoupleShould
+{
+    [Fact(DisplayName = "Calcul des impôts selon le salaire mensuel")]
+    public void CalculSelonSalairePrincipal()
+    {
+        string situationFamiliale = "Marié/Pacsé";
+        int salaireMensuelConjoint = 2500;
+        int nombreEnfants = 0;
+
+        int salaireMensuelPrincipal = 2000;
+        decimal resultat = Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+        resultat.Should().Be(4043.90m);
+
+        salaireMensuelPrincipal = 0;
+        Action action = () => Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+        action.Should().Throw<ArgumentException>();
+
+        salaireMensuelPrincipal = -2000;
+        action = () => Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact(DisplayName = "Calcul des impôts selon le salaire mensuel du conjoint")]
+    public void CalculSelonSalaireConjoint()
+    {
+        string situationFamiliale = "Marié/Pacsé";
+        int salaireMensuelPrincipal = 2000;
+        int nombreEnfants = 0;
+
+        int salaireMensuelConjoint = 2500;
+        decimal resultat = Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+        resultat.Should().Be(4043.90m);
+
+        salaireMensuelConjoint = -2000;
+        Action action = () => Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelConjoint,
+            salaireMensuelPrincipal,
+            nombreEnfants
+        );
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact(DisplayName = "Calcul des impôts selon le nombre d'enfants à charge")]
+    public void CalculSelonNombreEnfants()
+    {
+        string situationFamiliale = "Marié/Pacsé";
+        int salaireMensuelPrincipal = 2000;
+        int salaireMensuelConjoint = 2500;
+
+        int nombreEnfants = 0;
+        decimal resultat = Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+        resultat.Should().Be(4043.90m);
+
+        nombreEnfants = 5;
+        resultat = Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelPrincipal,
+            salaireMensuelConjoint,
+            nombreEnfants
+        );
+        resultat.Should().Be(878.62m);
+
+        nombreEnfants = -2;
+        Action action = () => Simulateur.CalculerImpotsAnnuel(
+            situationFamiliale,
+            salaireMensuelConjoint,
+            salaireMensuelPrincipal,
+            nombreEnfants
+        );
+
+        action.Should().Throw<ArgumentException>();
+    }
+}
