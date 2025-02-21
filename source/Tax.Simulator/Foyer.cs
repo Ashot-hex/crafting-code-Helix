2fd201d581b3c911691a017a3f395f38f54dce02
AshotManukyan <ashotm0571@gmail.com>
2025-02-21 16:50:31 +0100

    feat: add a model in Foyer.cs to simplify the calculations in Simulateur.cs

---
+++ source/Tax.Simulator/Foyer.cs
@@ -0,0 +1,93 @@
+﻿using Tax.Simulator.Exceptions;
+
+namespace Tax.Simulator;
+
+/// <summary>
+/// Model d'un foyer pour le calcul des impôts annuel
+/// </summary>
+public class Foyer
+{
+    private string situationFamiliale;
+    private decimal salairePrincipal;
+    private decimal salaireConjoint;
+    private int nombreEnfants;
+
+    /// <summary>
+    /// Constructeur par défaut
+    /// </summary>
+    /// <param name="situationFamiliale"> Situation familiale du foyer </param>
+    /// <param name="nombreEnfants"> Nombre d'enfants dans le foyer </param>
+    /// <param name="salairePrincipal"> Salaire principal du foyer </param>
+    /// <param name="salaireConjoint"> Salaire du conjoint, par default à 0 </param>
+    /// 
+    /// <exception cref="SituationFamilialeInvalide">Si la situation familiale est invalide</exception>
+    /// <exception cref="SalaireNegatif">Si le salaire principal, ou celui du conjoint, est invalide</exception>
+    /// <exception cref="NombreEnfantsInvalide">Si le nombre d'enfants donnés est invalide</exception>
+    public Foyer(string situationFamiliale, int nombreEnfants, decimal salairePrincipal, decimal salaireConjoint = 0)
+    {
+        this.situationFamiliale = situationFamiliale;
+        this.salairePrincipal = salairePrincipal;
+        this.salaireConjoint = salaireConjoint;
+        this.nombreEnfants = nombreEnfants;
+
+        VerifierValeurs();
+    }
+
+    /// <summary>
+    /// Verifie que tout les attributs ont étés correctement définit
+    /// </summary>
+    /// 
+    /// <exception cref="SituationFamilialeInvalide">Si la situation familiale est invalide</exception>
+    /// <exception cref="SalaireNegatif">Si le salaire principal, ou celui du conjoint, est invalide</exception>
+    /// <exception cref="NombreEnfantsInvalide">Si le nombre d'enfants donnés est invalide</exception>
+    private void VerifierValeurs()
+    {
+        if (situationFamiliale != "Célibataire" && situationFamiliale != "Marié/Pacsé")
+        {
+            throw new SituationFamilialeInvalide();
+        }
+
+        if (salairePrincipal <= 0)
+        {
+            throw new SalaireNegatif();
+        }
+
+        if (situationFamiliale == "Marié/Pacsé" && salaireConjoint < 0)
+        {
+            throw new SalaireNegatif();
+        }
+
+        if (nombreEnfants < 0)
+        {
+            throw new NombreEnfantsInvalide();
+        }
+    }
+
+    /// <summary>
+    /// Obtient le salaire total du foyer
+    /// </summary>
+    public decimal SalaireMensuelTotal => salairePrincipal + salaireConjoint;
+
+    /// <summary>
+    /// Obtient les parts fiscales du foyer
+    /// </summary>
+    public decimal PartsFiscales
+    {
+        get
+        {
+            int baseQuotient = situationFamiliale == "Marié/Pacsé" ? 2 : 1;
+            decimal quotientEnfants = 0m;
+
+            if (nombreEnfants <= 2)
+            {
+                quotientEnfants = nombreEnfants / 2m;
+            }
+            else
+            {
+                quotientEnfants = 1.0m + (nombreEnfants - 2) * 0.5m;
+            }
+
+            return baseQuotient + quotientEnfants;
+        }
+    }
+}
