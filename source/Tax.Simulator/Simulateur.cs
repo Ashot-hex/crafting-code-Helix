1c38a107925249b28e9c9f46dd0e276cdb38bd11
AshotManukyan <ashotm0571@gmail.com>
2025-02-21 17:32:37 +0100

    fix: add missing documentation in Simulateur.cs

--- source/Tax.Simulator/Simulateur.cs
+++ source/Tax.Simulator/Simulateur.cs
@@ -1,15 +1,16 @@
-using Tax.Simulator.Exceptions;
-
 namespace Tax.Simulator;
 
 public static class Simulateur
 {
     private const int MOIS_PAR_ANNEE = 12; //Nombre de mois dans l'année
-
     private static readonly decimal[] TRANCHES_IMPOSITION = { 10225m, 26070m, 74545m, 160336m }; //Plafonds des tranches
-
     private static readonly decimal[] TAUX_IMPOSITION = { 0.0m, 0.11m, 0.30m, 0.41m, 0.45m }; //Taux correspondants
 
+    /// <summary>
+    /// Calcule et revoit les impôts annuels à payer d'un foyer donné
+    /// </summary>
+    /// <param name="foyer">Foyer sur lequel les calculs doivent être fait</param>
+    /// <returns>La somme à payer, arrondit à deux décimales</returns>
     public static decimal CalculerImpotsAnnuel(Foyer foyer)
     {
         decimal revenuAnnuel = foyer.SalaireMensuelTotal * MOIS_PAR_ANNEE;
